4ff57b35ef3cb1998eb7cd97c87e327aa4dc4c55
MoReza <mz.abdollahi223@gmail.com>
2020-11-02 15:53:51 +0330

    Update Contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,7 +5,7 @@ const Contact = require('./../models/contactModel')
 
 exports.getContacts = async (req, res) => {
     try {
-        const contacts = await Contact.find({ user: req.user._id }).populate('user')
+        const contacts = await Contact.find({ user: req.user._id })
         res.status(200).json({
             status: 'success',
             contacts
@@ -39,5 +39,36 @@ exports.createContact = async (req, res) => {
     } catch (err) {
         console.log(err.message)
         res.status(500).json({ err: 'server error' })
+    }
+}
+
+exports.updateContact = async (req, res) => {
+    try {
+        //const contact = await Contact.findByIdAndUpdate(req.params.id, req.body)
+        const contact = await Contact.findById(req.params.id)
+
+        if (!contact) {
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Contact not found'
+            })
+        }
+
+        if (contact.user.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                status: 'fail',
+                msg: 'Not Authorized'
+            })
+        }
+
+        const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true })
+
+        res.status(200).json({
+            status: 'success',
+            contact: updatedContact
+        })
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({ status: 'error', err: 'server error' })
     }
 }
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -9,6 +9,6 @@ const router = express.Router()
 router.use(authController.isLogin)
 router.route('/').get(contactController.getContacts)
 router.route('/').post([body('name', 'Name is required').not().isEmpty()], contactController.createContact)
-
+router.route('/:id').put([body('name', 'Name is required').not().isEmpty()], contactController.updateContact)
 
 module.exports = router
