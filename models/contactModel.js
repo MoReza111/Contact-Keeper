4fcf6623b95018bf740f32789c3efc49bafb90ba
MoReza <mz.abdollahi223@gmail.com>
2020-11-02 14:57:29 +0330

    Contact Model is added

---
+++ models/contactModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+const contactSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String
+    },
+    type: {
+        type: String,
+        default: 'personal'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const contactModel = mongoose.model('contact', contactSchema)
