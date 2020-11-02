44011dbc93c4071b93173b71eb9c3d4f2c700eb7
MoReza <mz.abdollahi223@gmail.com>
2020-11-02 10:29:39 +0330

    Routers are defined

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 
+const userRouter = require('./routes/userRoutes')
+const authRouter = require('./routes/authRoutes')
+const contactRouter = require('./routes/contactRoutes')
+
 const app = express()
 
+// Define Routes
+app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/contacts', contactRouter)
 
 
 module.exports = app
