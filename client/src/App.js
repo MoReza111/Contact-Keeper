4a5f4563202e4bd630708da0151bdb47946a114e
MoReza <mz.abdollahi223@gmail.com>
2020-11-03 20:44:29 +0330

    Navbar is added to Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import React, { Fragment } from 'react'
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
